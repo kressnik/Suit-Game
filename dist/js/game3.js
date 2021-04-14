0009a6f19648eb549e0c7fa7e744aacd2b63287e
kressnik <bevz587@gmail.com>
2021-04-15 01:14:21 +0300

    feat(game3): created class GridDraw

--- dist/js/game3.js
+++ dist/js/game3.js
@@ -4,8 +4,22 @@ $(function () {
     const gridArr = grid.create();
 
     const gridModificator = new GridModificator(gridArr);
+    const gridDraw = new GridDraw(gridArr, '#game-3 .game-box__lines', 'game-box__line', 'game-box__suit');
 
-    console.log(gridModificator.change(1,1));
+    gridDraw.clearBody();
+    gridDraw.create();
+
+    $(`#game-3 .game-box__suit`).on('click', clickHandler);
+
+    function clickHandler() {
+        let line = $(this).parent().data('key-line');
+        let element = $(this).data('key-element');
+        let newGrid = gridModificator.change(line, element);
+
+        gridDraw.update(newGrid);
+
+    }
+    //console.log(gridModificator.change(1,1));
 
 });
 
@@ -42,7 +56,8 @@ class GridModificator {
 
     change(selectedKeyLine, selectedKeyElement) {
         this.selectedElement = this.grid[selectedKeyLine][selectedKeyElement];
-        this.dfs(selectedKeyLine, selectedKeyElement);
+        
+        if(this.selectedElement) this.dfs(selectedKeyLine, selectedKeyElement);
 
         return JSON.parse(JSON.stringify(this.grid));
     }
@@ -72,5 +87,56 @@ class GridModificator {
 
     checkSizeGrid(qtyLine, qtyElement, nextKeyLine, nextKeySuit) {
         return 0 <= nextKeyLine && nextKeyLine < qtyLine && 0 <= nextKeySuit && nextKeySuit < qtyElement;
+    }
+}
+
+class GridDraw {
+    constructor(grid = null, boxID = null, lineClass = null, elementClass = null) {
+        this.grid = grid;
+        this.boxID = boxID;
+        this.lineClass = lineClass;
+        this.elementClass = elementClass;
+    }
+
+    create() {
+        const boxBody = $(`${this.boxID}`);
+
+        for (const [key, elsements] of this.grid.entries()) {
+            const newLine = this.createLine(key);
+            for (const [key, element] of elsements.entries()) {
+                const newElement = this.createElement(key, element);
+                newLine.append(newElement);
+            }
+            boxBody.append(newLine);
+        }
+    }
+
+    update(grid) {
+        this.grid = grid;
+
+        const boxBody = $(`${this.boxID}`);
+
+        for (const [key, elsements] of this.grid.entries()) {
+            const line = boxBody.find(`.${this.lineClass}:eq(${key})`);
+            for (const [key, element] of elsements.entries()) {
+                const text = element ? `&${element};` : '';
+                line.find(`:eq(${key})`).html(text);
+            }
+        }
+    }
+
+    createLine(keyLine = null) {
+        return $('<div/>').addClass(this.lineClass).data('key-line', keyLine);
+    }
+
+    createElement(keyElement = null, element = null) {
+        const $newElement = $('<span/>').addClass(this.elementClass).data('key-element', keyElement);
+        const text = element ? `&${element};` : '';
+
+        return $newElement.addClass(element).html(text);
+    }
+
+    clearBody() {
+        $(`${this.boxID}`).empty();
     }
 }
