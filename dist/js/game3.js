9e22d65aeb1fce0c4a338fc26921df4f626ed586
kressnik <bevz587@gmail.com>
2021-04-15 00:05:31 +0300

    feat(game3): created class GridModificator

--- dist/js/game3.js
+++ dist/js/game3.js
@@ -2,7 +2,13 @@
 $(function () {
     const grid = new Grid(10, 10);
     const gridArr = grid.create();
+
+    const gridModificator = new GridModificator(gridArr);
+
+    console.log(gridModificator.change(1,1));
+
 });
+
 class Grid {
     constructor(qtyLine = null, qtyElement = null, elementArr = null) {
         this.grid = [];
@@ -26,3 +32,45 @@ class Grid {
         return JSON.parse(JSON.stringify(this.grid));
     }
 } 
+
+class GridModificator {
+    constructor(grid = null, changeToElement = null) {
+        this.grid = grid;
+        this.selectedElement = '';
+        this.changeToElement = changeToElement || '';
+    }
+
+    change(selectedKeyLine, selectedKeyElement) {
+        this.selectedElement = this.grid[selectedKeyLine][selectedKeyElement];
+        this.dfs(selectedKeyLine, selectedKeyElement);
+
+        return JSON.parse(JSON.stringify(this.grid));
+    }
+
+    dfs(selectedKeyLine, selectedKeyElement) {
+        const right = [0, 1];
+        const up = [1, 0];
+        const left = [0, -1];
+        const down = [-1, 0];
+
+        this.grid[selectedKeyLine][selectedKeyElement] = this.changeToElement;
+
+        for (const [suitIndex, lineIndex] of [right, up, left, down]) {
+            const nextKeyLine = selectedKeyLine + lineIndex;
+            const nextKeyElement = selectedKeyElement + suitIndex;
+            const qtyLine = this.grid.length;
+            const qtyElement = this.grid[0].length;
+            const resultSizeGrid = this.checkSizeGrid(qtyLine, qtyElement, nextKeyLine, nextKeyElement);
+            
+            if (resultSizeGrid && this.grid[nextKeyLine][nextKeyElement] === this.selectedElement) {
+                this.dfs(nextKeyLine, nextKeyElement)
+            }
+        }
+
+        return true;
+    }
+
+    checkSizeGrid(qtyLine, qtyElement, nextKeyLine, nextKeySuit) {
+        return 0 <= nextKeyLine && nextKeyLine < qtyLine && 0 <= nextKeySuit && nextKeySuit < qtyElement;
+    }
+}
