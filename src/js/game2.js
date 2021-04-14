1dc7833f15eb67d804c26db3707cb63edbca484c
kressnik <bevz587@gmail.com>
2021-04-14 18:03:41 +0300

    feat(game2): added click to playing card suit

--- src/js/game2.js
+++ src/js/game2.js
@@ -1,6 +1,8 @@
 $(function () {
     const ID_GAME = 'game-2';
-    const suitGrid = [
+    const SUIT_ELEMENT = 'game-box__suit';
+    const SUIT = ['spades', 'clubs', 'diams', 'hearts'];
+    const SUIT_GRID = [
         ['spades', 'spades', 'clubs', 'diams', 'clubs', 'clubs'],
         ['spades', 'spades', 'clubs', 'diams', 'diams', 'diams'],
         ['spades', 'clubs', 'clubs', 'diams', 'diams', 'diams'],
@@ -10,22 +12,71 @@ $(function () {
         ['hearts', 'hearts', 'hearts', 'spades', 'spades', 'clubs']
     ];
 
-    const $gameBoxLines = $(`#${ID_GAME} .game-box__lines`);
-    const $prototypeLine = $('<div/>').addClass('game-box__line');
-    const $prototypeSuit = $('<span/>').addClass('game-box__suit');
+    drawnPlayingCard(SUIT_GRID);
 
-    $gameBoxLines.empty();
+    function clickHandler() {
+        let lineIndex = $(this).parent().data('line-index');
+        let suitIndex = $(this).data('suit-index');
+        let suit = SUIT_GRID[lineIndex][suitIndex];
 
-    for (const suitLine of suitGrid) {
-        let $newLine = $prototypeLine.clone();
-
-        for (const suit of suitLine) {
-            let $newSuit = $prototypeSuit.clone();
-
-            $newSuit.addClass(suit).html(`&${suit};`);
-            $newLine.append($newSuit);
+        if (SUIT.indexOf(suit) !== -1) {
+            let newSuitGrid = dfs(suit, SUIT_GRID, lineIndex, suitIndex);
+            drawnPlayingCard(newSuitGrid);
         }
 
-        $gameBoxLines.append($newLine);
+    }
+
+    function drawnPlayingCard(grid) {
+
+        const $gameBoxLines = $(`#${ID_GAME} .game-box__lines`);
+        const $prototypeLine = $('<div/>').addClass('game-box__line');
+        const $prototypeSuit = $('<span/>').addClass(SUIT_ELEMENT);
+
+        $gameBoxLines.empty();
+
+        for (const [index, suitLine] of grid.entries()) {
+            let $newLine = $prototypeLine.clone().data('line-index', index);
+
+            for (const [index, suit] of suitLine.entries()) {
+                let $newSuit = $prototypeSuit.clone().data('suit-index', index);
+                let text = suit ? `&${suit};` : '';
+
+                $newSuit.addClass(suit).html(text);
+                $newLine.append($newSuit);
+            }
+
+            $gameBoxLines.append($newLine);
+        }
+
+        $(`#${ID_GAME} .${SUIT_ELEMENT}`).on('click', clickHandler);
+    }
+
+
+    function dfs(currentSuit, suitGrid, currentLineIndex, currentSuitIndex) {
+        suitGrid[currentLineIndex][currentSuitIndex] = '';
+
+        const right = [0, 1];
+        const up = [1, 0];
+        const left = [0, -1];
+        const down = [-1, 0];
+
+        for (const [suitIndex, lineIndex] of [right, up, left, down]) {
+            const nextLineIndex = currentLineIndex + lineIndex;
+            const nextSuitIndex = currentSuitIndex + suitIndex;
+            const quantityLine = suitGrid.length;
+            const quantitySuit = suitGrid[0].length;
+            const resultSizeGrid = checkSizeGrid(quantityLine, quantitySuit, nextLineIndex, nextSuitIndex);
+
+
+            if (resultSizeGrid && suitGrid[nextLineIndex][nextSuitIndex] === currentSuit) {
+                dfs(currentSuit, suitGrid, nextLineIndex, nextSuitIndex)
+            }
+        }
+
+        return suitGrid;
+    }
+
+    function checkSizeGrid(quantityLine, quantitySuit, nextLineIndex, nextSuitIndex) {
+        return 0 <= nextLineIndex && nextLineIndex < quantityLine && 0 <= nextSuitIndex && nextSuitIndex < quantitySuit;
     }
 });
