b227280b21bfe457ea0fbc1fd66364965e0c0858
kressnik <bevz587@gmail.com>
2021-04-15 00:04:07 +0300

    feat(game3): created class grid

--- dist/js/game3.js
+++ dist/js/game3.js
@@ -1,1 +1,28 @@
 'use strict';
+$(function () {
+    const grid = new Grid(10, 10);
+    const gridArr = grid.create();
+});
+class Grid {
+    constructor(qtyLine = null, qtyElement = null, elementArr = null) {
+        this.grid = [];
+        this.qtyLine = qtyLine || 7;
+        this.qtyElement = qtyElement || 6;
+        this.elementArr = elementArr || ['spades', 'clubs', 'diams', 'hearts']
+    }
+
+    create() {
+        for (let line = 0; line < this.qtyLine; line++) {
+            this.grid[line] = [];
+            for (let index = 0; index < this.qtyElement; index++) {
+                this.grid[line].push(this.elementArr.randElement());
+            }
+        }
+
+        return JSON.parse(JSON.stringify(this.grid));
+    }
+
+    get() {
+        return JSON.parse(JSON.stringify(this.grid));
+    }
+} 
