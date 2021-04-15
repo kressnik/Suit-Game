f12473d2fde2c3737d8cc084f6c09a154d3fb19b
kressnik <bevz587@gmail.com>
2021-04-15 14:12:21 +0300

    feat(game3) added change suit

--- dist/js/game3.js
+++ dist/js/game3.js
@@ -1,26 +1,36 @@
 'use strict';
 $(function () {
-    const grid = new Grid(10, 10);
+    const grid = new Grid();
     const gridArr = grid.create();
+    const getSuit = grid.getNextElement();
+    const gridModificator = new GridModificator(gridArr, getSuit());
 
-    const gridModificator = new GridModificator(gridArr);
-    const gridDraw = new GridDraw(gridArr, '#game-3 .game-box__lines', 'game-box__line', 'game-box__suit');
 
+    const dataDraw = {
+        grid: gridArr,
+        boxId: '#game-3',
+        boxBody: '.game-box__lines',
+        lineClass: '.game-box__line',
+        elementClass: '.game-box__suit',
+        boxElement: '.game-box__next-element'
+    }
+    const gridDraw = new GridDraw(dataDraw);
+    gridDraw.printNextElement(getSuit());
     gridDraw.clearBody();
     gridDraw.create();
-
-    $(`#game-3 .game-box__suit`).on('click', clickHandler);
 
     function clickHandler() {
         let line = $(this).parent().data('key-line');
         let element = $(this).data('key-element');
         let newGrid = gridModificator.change(line, element);
-
         gridDraw.update(newGrid);
 
+        let getSuit = grid.getNextElement();
+        gridModificator.changeElement(getSuit());
+        gridDraw.printNextElement(getSuit());
     }
-    //console.log(gridModificator.change(1,1));
 
+    $(`#game-3 .game-box__suit`).on('click', clickHandler);
 });
 
 class Grid {
@@ -28,7 +38,7 @@ class Grid {
         this.grid = [];
         this.qtyLine = qtyLine || 7;
         this.qtyElement = qtyElement || 6;
-        this.elementArr = elementArr || ['spades', 'clubs', 'diams', 'hearts']
+        this.elementArr = elementArr || ['spades', 'clubs', 'diams', 'hearts'];
     }
 
     create() {
@@ -45,7 +55,16 @@ class Grid {
     get() {
         return JSON.parse(JSON.stringify(this.grid));
     }
-} 
+
+    nextElement() {
+        return this.elementArr.randElement();
+    }
+
+    getNextElement() {
+        const next = this.elementArr.randElement();
+        return () => next;
+    }
+}
 
 class GridModificator {
     constructor(grid = null, changeToElement = null) {
@@ -56,10 +75,16 @@ class GridModificator {
 
     change(selectedKeyLine, selectedKeyElement) {
         this.selectedElement = this.grid[selectedKeyLine][selectedKeyElement];
-        
-        if(this.selectedElement) this.dfs(selectedKeyLine, selectedKeyElement);
+
+        if (this.selectedElement !== this.changeToElement) {
+            this.dfs(selectedKeyLine, selectedKeyElement)
+        };
 
         return JSON.parse(JSON.stringify(this.grid));
+    }
+
+    changeElement(element) {
+        this.changeToElement = element;
     }
 
     dfs(selectedKeyLine, selectedKeyElement) {
@@ -76,7 +101,7 @@ class GridModificator {
             const qtyLine = this.grid.length;
             const qtyElement = this.grid[0].length;
             const resultSizeGrid = this.checkSizeGrid(qtyLine, qtyElement, nextKeyLine, nextKeyElement);
-            
+
             if (resultSizeGrid && this.grid[nextKeyLine][nextKeyElement] === this.selectedElement) {
                 this.dfs(nextKeyLine, nextKeyElement)
             }
@@ -91,15 +116,24 @@ class GridModificator {
 }
 
 class GridDraw {
-    constructor(grid = null, boxID = null, lineClass = null, elementClass = null) {
+    constructor({ 
+        grid = null, 
+        boxId = null, 
+        boxBody = null,
+        lineClass = null, 
+        elementClass = null,
+        boxElement = null
+    }) {
         this.grid = grid;
-        this.boxID = boxID;
+        this.boxId = boxId;
+        this.boxBody = boxBody,
         this.lineClass = lineClass;
         this.elementClass = elementClass;
+        this.boxElement = boxElement;
     }
 
     create() {
-        const boxBody = $(`${this.boxID}`);
+        const boxBody = $(`${this.boxId} ${this.boxBody}`);
 
         for (const [key, elsements] of this.grid.entries()) {
             const newLine = this.createLine(key);
@@ -114,29 +148,41 @@ class GridDraw {
     update(grid) {
         this.grid = grid;
 
-        const boxBody = $(`${this.boxID}`);
+        const boxBody = $(`${this.boxId} ${this.boxBody}`);
 
         for (const [key, elsements] of this.grid.entries()) {
-            const line = boxBody.find(`.${this.lineClass}:eq(${key})`);
+            const line = boxBody.find(`${this.lineClass}:eq(${key})`);
             for (const [key, element] of elsements.entries()) {
                 const text = element ? `&${element};` : '';
-                line.find(`:eq(${key})`).html(text);
+                line
+                    .find(`:eq(${key})`)
+                    .removeClass()
+                    .addClass(`${element} ${this.elementClass.slice(1)}`)
+                    .html(text);
             }
         }
     }
 
     createLine(keyLine = null) {
-        return $('<div/>').addClass(this.lineClass).data('key-line', keyLine);
+        return $('<div/>').addClass(this.lineClass.slice(1)).data('key-line', keyLine);
     }
 
     createElement(keyElement = null, element = null) {
-        const $newElement = $('<span/>').addClass(this.elementClass).data('key-element', keyElement);
+        const $newElement = $('<span/>').addClass(this.elementClass.slice(1)).data('key-element', keyElement);
         const text = element ? `&${element};` : '';
 
         return $newElement.addClass(element).html(text);
     }
 
+    printNextElement(element) {
+        const text = element ? `&${element};` : '';
+        $(`${this.boxId} ${this.boxElement} i`)
+            .removeClass()
+            .addClass(element)
+            .html(text);
+    }
+
     clearBody() {
-        $(`${this.boxID}`).empty();
+        $(`${this.boxId} ${this.boxBody}`).empty();
     }
 }
